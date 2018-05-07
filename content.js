8046249d3e4881f50b6a3e391af274119d357f43
Bewelge <benni@Bennis-MacBook-Pro.local>
2018-05-08 00:38:23 +0200

    -added link to repo in readMe -now supports old.reddit.com -added warning that it doesn’t work on redesign

--- content.js
+++ content.js
@@ -1,16 +1,17 @@
 //this part is executed before the DOM starts loading.
 //If we are on reddit.com ('best')
-if ("https://www.reddit.com/" == window.location.href) // if it matches pattern defined above
+if ("https://www.reddit.com/" == window.location.href || "http://www.reddit.com/" == window.location.href || "https://old.reddit.com/" == window.location.href || "https://old.reddit.com/" == window.location.href)
 {
 	//redirect to /hot
 	window.location.href = "/hot";
 }
+
 //This part is executed after the page has loaded.
 //This is necessary to change the links of the home button in the top bar and the Snoo-link.
 window.onload=function() {
 
 	//check if we are on reddit.com
-	if (window.location.href.split("reddit.com").length>1) {
+	if (window.location.href.split("reddit.c").length>1) {
 
 		//Some subreddits have the Snoo-link in "header-img" some in "header-img-a". So we're simply trying to change both 
 		//with these extremely unprofessional try-catch blocks :-).
@@ -28,7 +29,7 @@ window.onload=function() {
 		//change home-link in top-bar.
 		let choices = document.getElementsByClassName("choice");
 		for (let key in choices) {
-			if (choices[key].href == "https://www.reddit.com/") {
+			if (choices[key].href == "https://www.reddit.com/" || choices[key].href == "https://old.reddit.com/") {
 				if (choices[key].innerHTML == "home") {
 
 				} else if ( choices[key].innerHTML == "best")
